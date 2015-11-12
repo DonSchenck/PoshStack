3af01d0e17c69778a28acdd50bf7d7db798208ae
Don Schenck <don.schenck@gmail.com>
2015-11-12 13:33:17 -0500

    Fixes to build.ps1

--- Scripts/Build.ps1
+++ Scripts/Build.ps1
@@ -1,5 +1,17 @@
 #
 # Build.ps1
 #
+
+# If NUGET is NOT installed on the build machine, install it here and now.
+if(!( test-path $env:LocalAppData\NuGet\NuGet.exe)) {
+  # download nuget
+  Invoke-WebRequest 'https://www.nuget.org/nuget.exe' -OutFile "$($env:LocalAppData)\NuGet\NuGet.exe"
+}
+
 # Run PACK to build the Nuget package
-nuget pack ..\PoshStack.nuspec -NoPackageAnalysis
+$CMD = "$env:LocalAppData\NuGet\NuGet.exe"
+$arg1 = 'pack'
+$arg2 = '..\PoshStack.nuspec'
+$arg3 = "-NoPackageAnalysis"
+
+& $CMD $arg1 $arg2 $arg3
