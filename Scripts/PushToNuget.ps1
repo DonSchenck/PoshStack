498c70298a6e3db84a19e360f78fa69c978cd0a4
Don Schenck <don.schenck@gmail.com>
2015-11-12 16:18:28 -0500

    Added script to push to Nuget (for Bamboo)

---
+++ Scripts/PushToNuget.ps1
@@ -0,0 +1,17 @@
+#
+# PushToNuget.ps1
+#
+
+# First, we need to find the name of the package
+$PackageFile = Get-ChildItem -Filter *.nupkg | Select-Object -First 1
+
+# If null or empty, bail immediately
+
+
+# Now we can push it to nuget.org using the NuGet command line tool
+$CMD = "$env:LocalAppData\NuGet\NuGet.exe"
+$arg1 = 'push'
+$arg2 = $PackageFile.Name
+$arg3 = "%NUGET_APIKEY_PASSWORD%"
+
+& $CMD $arg1 $arg2 $arg3
