859aa4cf699a259c39736e385439aed485fb5595
Don Schenck <don.schenck@gmail.com>
2015-12-03 15:14:56 -0500

    Fixes #371, fixes #385

--- Examples/Example_CloudQueues.ps1
+++ Examples/Example_CloudQueues.ps1
@@ -1,12 +1,23 @@
 ﻿Import-Module PoshStack
+
 $myMetadata = @{}
 $myMetadata.Add("Hello","World")
 $myMetadata.Add("Foo","Bar")
 #New-OpenStackCloudQueue -Account rackiad -QueueName "queuename" -RegionOverride IAD
 #Get-OpenStackCloudQueue -Account rackiad -RegionOverride IAD
 #Set-OpenStackCloudQueueMetadata -Account rackiad -QueueName "queuename" -Metadata $myMetadata -RegionOverride IAD
-$md = Get-OpenStackCloudQueueMetadata -Account rackiad -QueueName "queuename" -RegionOverride IAD
-ForEach($metadata in $md) {
-    Write-Host $metadata.keys
-    Write-Host $metadata.Values
+#$md = Get-OpenStackCloudQueueMetadata -Account rackiad -QueueName "queuename" -RegionOverride IAD
+#ForEach($metadata in $md) {
+#    Write-Host $metadata.keys
+#    Write-Host $metadata.Values
+#}
+$TTL = New-Object ([TimeSpan]) 4,0,0
+$listofmsgs = @()
+$listofmsgs += "Hello world"
+$listofmsgs += "Second message"
+#Write-OpenStackCloudQueueMessage -Account rackiad -QueueName "queuename" -TTL $TTL -ListOfMessages $listofmsgs -RegionOverride IAD
+$readmessages = Read-OpenStackCloudQueueMessage -Account rackiad -Queuename "queuename" -TTL $TTL -GracePeriod $TTL -NumberToRetrieve 4 -RegionOverride IAD
+Write-Host $readmessages.Messages.Count
+ForEach($m in $readmessages.Messages) {
+      Write-Host "body " $m.Body.ToString()
 }
